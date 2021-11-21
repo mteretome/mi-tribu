039e61ff5b7ddb3f7f9394c3ff6e17283e0becb6
Yinon-Shirazi <ys3440@columbia.edu>
2021-11-21 13:05:22 -0500

    Calculated all dates for MVP login

--- mvp/src/context/actions/welcome.js
+++ mvp/src/context/actions/welcome.js
@@ -1,6 +1,13 @@
 import axiosInstance from "../../config/axiosInterceptor";
 import { CLEAR_AUTH_STATE,CLEAR_MODE,DASH_CLEAR,WELCOME_SUCCESS } from "../../constants/actions";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
+//import { extendMoment } from 'moment-range';
+
+
+
+
+
 export const clearAuthState =()=> dispatch=> {
     dispatch({
         type: CLEAR_AUTH_STATE,
@@ -21,9 +28,33 @@ export const dashboardClear =()=> dispatch=> {
 
 export const welcome =({name, due_date})=> (dispatch)=> (onSucces)=> {
     //calculate week date if successful
-    const week_dates = {"5":"dummy dates", "6":"dummy"};
-    const day = 7;
-    const current_week = 8;
+
+    const today = new Date();
+    const period_date = moment(due_date).add(-280, 'day').format('YYYY-MM-DD');
+    var difference = new Date(today).getTime() - new Date(period_date).getTime();  
+    var difference_in_days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const current_week = Math.floor(difference_in_days/7);
+    const day = difference_in_days%7;
+
+    const week_dates = {};
+
+    var week = 42;
+    var offset = 14;
+    while(week > 4){
+        week_dates[week] = moment(due_date).add(offset, 'day').format('YYYY-MM-DD');
+        offset = offset -7;
+        week = week -1;
+    }
+    
+
+    console.log("The due date is -->", due_date);
+    console.log("The last period is -->", period_date);
+    console.log("The current week is  -->", current_week);
+    console.log("The day in the current week is -->", day);
+    
+
+
+
     
     //success
     //else fail
@@ -37,4 +68,8 @@ export const welcome =({name, due_date})=> (dispatch)=> (onSucces)=> {
     AsyncStorage.setItem("day",JSON.stringify(day));
     AsyncStorage.setItem("current_week",JSON.stringify(current_week));
     onSucces({name,due_date,week_dates,day,current_week});
+
+  
 };
+
+
